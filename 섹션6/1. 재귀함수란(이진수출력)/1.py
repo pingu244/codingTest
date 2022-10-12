894c2f783c25ca71043641841988ac94420e62aa
pingu244 <69796976+pingu244@users.noreply.github.com>
2022-10-12 17:46:37 +0900

    10/12

---
+++ 섹션6/1. 재귀함수란(이진수출력)/1.py
@@ -0,0 +1,13 @@
+import sys
+#sys.stdin = open("input.txt", 'r')
+
+def recursion(n):
+    if n>=2:
+        recursion(n//2)
+        #print(n)
+        print(n%2, end='')
+    elif n==1:
+        print(1, end='')
+
+n=int(input())
+recursion(n)
